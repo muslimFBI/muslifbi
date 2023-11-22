405eb941c3bdad81ff682b194096c41c567b61a9
muslimFBI <151764874+muslimFBI@users.noreply.github.com>
2023-11-22 10:56:04 -0500

    Add files via upload

---
+++ muslim.py
@@ -0,0 +1,10 @@
+son=int(input("Yilingizni kiriting : "))
+if  son == 15 :
+    print ("Hali godaksan bratishka")
+elif son >=17 :
+    print ("Id karta olasan")
+else :
+    print ("Tori yoz darmaed")
+son1 = range (1, 101, 1)
+for son2 in son1:
+    print(son2)
