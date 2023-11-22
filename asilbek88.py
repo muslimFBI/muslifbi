cdd6f8e2cbce94fa5e90d9970061baa74af8265a
muslimFBI <151764874+muslimFBI@users.noreply.github.com>
2023-11-22 10:50:39 -0500

    Add files via upload

---
+++ asilbek88.py
@@ -0,0 +1,13 @@
+"""kurs=["kamolov','toshtemirov','oripov','azimov"]
+sonlar=['1,2,3,4']
+yoshlar=['16,15,14,13']
+ismlar=["nursulton',muslimbek',abdurauf,'asilbek"]
+mashina_modelar=['mrsades','bmw','kia','nexia']
+narhlar=['12345,45678,36765,6789']
+print(narhlar)
+"""
+
+
+
+"oppend"(0)
+ismlar=[azizbek]
